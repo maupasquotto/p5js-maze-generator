fe44f662cb6f71d458f02162f99f61539fd28cb0
Mauricio Pasquotto <maupasquotto@gmail.com>
2019-04-19 18:14:14 -0300

    Picking a start node and end node

--- node.js
+++ node.js
@@ -4,10 +4,15 @@ class Node {
         this.y = y;
         this.h = h;
         this.w = w;
+        this.color = 255;
+    }
+
+    setColor(color) {
+        this.color = color;
     }
 
     draw() {
-        fill(255);
+        fill(this.color);
         square(this.x, this.y, this.h);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
-let rows = 10;
-let cols = 10;
+/* https://en.wikipedia.org/wiki/Maze_generation_algorithm */
+
+let rows = 100;
+let cols = 100;
 let h = 0;
 let w = 0;
-
-let r = 0;
-let g = 0;
-let b = 0;
-let hue = 0;
-
 let nodes;
+
+let startNode;
+let endNode;
 
 function setup() {
     /* Canvas */
@@ -31,8 +30,20 @@ function setup() {
         nodes[i] = new Array(rows);
         for (let j = 0; j < nodes[i].length; j++) {
             nodes[i][j] = new Node(w * i, h * j, h, w);
+
+            /* Set baudrier color */
+            if (i == 0 || j == 0 || i == nodes.length -1 || j == nodes[i].length - 1) {
+                nodes[i][j].setColor(color(19, 15, 64));
+            }
         }
     }
+
+    /* Create start and end nodes */
+    /* The random starts at 1 and end before de boundary */
+    startNode = nodes[0][floor(random(1, cols))];
+    endNode = nodes[cols-1][floor(random(1, rows))];
+    startNode.setColor(color(104, 109, 224));
+    endNode.setColor(color(149, 175, 192));
 }
 
 function draw() {
