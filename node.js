f647f7c95d3bb6bbbfc2870ccc1245f43cceb90d
Mauricio Pasquotto <maupasquotto@gmail.com>
2019-06-06 21:51:10 -0300

    Aldus-Broder first implementation

--- node.js
+++ node.js
@@ -5,6 +5,10 @@ class Node {
         this.h = h;
         this.w = w;
         this.color = 255;
+
+        /* Maze */
+        this.wallDirections = ['S', 'W', 'E', 'N'];
+        this.visited = false;
     }
 
     setColor(color) {
@@ -12,7 +16,38 @@ class Node {
     }
 
     draw() {
+        push();
         fill(this.color);
+        noStroke();
         square(this.x, this.y, this.h);
+        pop();
+
+        /* Draw borders */
+        if (this.wallDirections.indexOf('W') !== -1) {
+            line(this.x, this.y, this.x, this.y + this.h);
+        }
+
+        if (this.wallDirections.indexOf('N') !== -1) {
+            line(this.x, this.y, this.x + this.w, this.y);
+        }
+
+        if (this.wallDirections.indexOf('E') !== -1) {
+            line(this.x + this.w, this.y, this.x + this.w, this.y + this.h);
+        }
+
+        if (this.wallDirections.indexOf('S') !== -1) {
+            line(this.x, this.y + this.h, this.x + this.w, this.y + this.h);
+        }
+    }
+
+    removeWall(wall) {
+        let index = this.wallDirections.indexOf(wall.toUpperCase());
+        if (index > -1) {
+            delete this.wallDirections[index];
+        }
+    }
+
+    setVisited(visited) {
+        this.visited = !!visited;
     }
 }
--- sketch.js
+++ sketch.js
@@ -9,7 +9,8 @@ let nodes;
 let startNode;
 let endNode;
 
-function setup() {
+function setup() 
+{
     /* Canvas */
     createCanvas(900, 900);
     background(0);
@@ -26,24 +27,64 @@ function setup() {
             nodes[i][j] = new Node(x * i, y * j, y, x);
 
             /* Set baudrier color */
-            if (i == 0 || j == 0 || i == nodes.length -1 || j == nodes[i].length - 1) {
-                nodes[i][j].setColor(color(19, 15, 64));
-            }
+            // if (i == 0 || j == 0 || i == nodes.length -1 || j == nodes[i].length - 1) {
+            //     nodes[i][j].setColor(color(19, 15, 64));
+            // }
         }
     }
 
     /* Create start and end nodes */
     /* The random starts at 1 and end before de boundary */
-    startNode = nodes[0][floor(random(1, cols))];
-    endNode = nodes[cols-1][floor(random(1, rows))];
-    startNode.setColor(color(104, 109, 224));
-    endNode.setColor(color(149, 175, 192));
+    // startNode = nodes[0][floor(random(1, cols))];
+    // endNode = nodes[cols-1][floor(random(1, rows))];
+    // startNode.setColor(color(104, 109, 224));
+    // endNode.setColor(color(149, 175, 192));
+
+    implementsMaze();
 
     /* Debug porpuses */
     noLoop();
 }
 
-function draw() {
+//Aldous-Broder Algorithm
+function implementsMaze()
+{
+    let mX = floor(random(0, rows+1)); // Plus one to include the last row/column
+    let mY = floor(random(0, cols+1)); // Plus one to include the last row/column
+    let DX = {'E': 1, 'W': -1, 'N': 0, 'S': 0};
+    let DY = {'E': 0, 'W': 0, 'N': -1, 'S': 1};
+    let opposite = {'E': 'W', 'W': 'E', 'N': 'S', 'S': 'N'};
+    let remaining = rows * cols;
+    let directions = ['N', 'S', 'E', 'W'];
+    let nx = 0, ny = 0;
+
+    while (remaining > 0) {
+        directions = shuffle(directions);
+
+        for(let i = 0; i < directions.length; i++) {
+            nx = mX + DX[directions[i]];
+            ny = mY + DY[directions[i]];
+            if (nx >= 0 && ny >= 0 && nx < cols && ny < rows) {
+                if (!nodes[nx][ny].visited) {
+                    nodes[nx][ny].setColor(color(255,0,0));
+                    nodes[nx][ny].setVisited(true);
+
+                    nodes[mX][mY].removeWall(directions[i]);
+                    nodes[nx][ny].removeWall(opposite[directions[i]]);
+                    
+                    remaining--;
+                }
+
+                mX = nx;
+                mY = ny;
+                break;
+            }
+        }
+    }
+}
+
+function draw() 
+{
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             nodes[i][j].draw();
